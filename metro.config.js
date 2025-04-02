de4b576ef34173f83427a0962b2409cac4e9d631
mekhiStarcks <122574138+mekhiStarcks@users.noreply.github.com>
2025-04-02 18:52:49 -0400

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,17 @@
-// metro.config.js
+const { getDefaultConfig } = require('metro-config');
 const path = require('path');
 
-module.exports = {
-  resolver: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'), // Map `@` to the `src` folder
+module.exports = async () => {
+  const {
+    resolver: { sourceExts },
+  } = await getDefaultConfig();
+
+  return {
+    resolver: {
+      sourceExts: [...sourceExts, 'ts', 'tsx', 'js', 'jsx'], // Add the extensions for TypeScript and JSX
+      extraNodeModules: {
+        '@': path.resolve(__dirname, 'app'), // Map '@' to the 'app' directory
+      },
     },
-  },
+  };
 };
