979c21f15b2df05948e0fe535360a20f413c3b13
mekhiStarcks <122574138+mekhiStarcks@users.noreply.github.com>
2025-04-02 18:46:27 -0400

    Create babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    presets: [
+      'module:metro-react-native-babel-preset',
+      '@babel/preset-react', // Add this line
+    ],
+    plugins: [
+      // You can add other plugins if necessary
+    ],
+  };
